8ade2d60c7b2d0ec2a419e2eff16ea2f07155204
Michael D Johnson <redbeard@mdjohnson.us>
2016-01-22 04:36:22 -0600

    Add docs for Equip framework

---
+++ equipframework.readthedocs.org/en/latest/readthedocs-data.js
@@ -0,0 +1,20 @@
+var READTHEDOCS_DATA = {
+    "project": "equipframework", 
+    "theme": "readthedocs", 
+    "version": "latest", 
+    "source_suffix": ".md", 
+    "api_host": "https://readthedocs.org/", 
+    "language": "en", 
+    "commit": "118e9ae58eb9f60d2ab2d43fa18c521f18745ff2", 
+    "docroot": "/home/docs/checkouts/readthedocs.org/user_builds/equipframework/checkouts/latest/docs", 
+    "builder": "mkdocs", 
+    "page": null
+}
+
+// Old variables
+var doc_version = "latest";
+var doc_slug = "equipframework";
+var page_name = "None";
+var html_theme = "readthedocs";
+
+READTHEDOCS_DATA["page"] = mkdocs_page_name
